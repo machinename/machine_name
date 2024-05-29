8a63cedaa385220ab8d90c769a7c141bdb30b4d0
K-Henry <dev@machinename.dev>
2024-05-28 22:31:10 -0500

    Removed Blog

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -6,19 +6,19 @@ import Link from 'next/link';
 export default function AppBar() {
     return (
         <Box className={styles.appBar} sx={{ backgroundColor: 'background.default' }}>
-            <Box className={styles.appBarContent}>
-                <Link href="/">
+            <Box className={styles.appBarTitle}>
+            <Link href="/">
                     <Button sx={{ borderRadius: '0'  }}>MACHINE NAME</Button>
                 </Link>
+            </Box>
+            <Box className={styles.appBarContent}>
                 <Link href="/mini_ml" >
                     <Button sx={{ borderRadius: '0' }}>MINI ML</Button>
                 </Link>
                 <Link href="/solutions" >
                     <Button sx={{ borderRadius: '0' }}>SOLUTIONS</Button>
                 </Link>
-                <Link href="/blog" >
-                    <Button sx={{ borderRadius: '0' }}>BLOG</Button>
-                </Link>
+ 
             </Box>
             <Box className={styles.appBarEnd} />
         </Box>
--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -13,17 +13,17 @@ export default function Solutions() {
             <h2 className={styles.h2}>Tailored Tech Solutions</h2>
             <div className={styles.container}>
                 <h3 className={styles.h3}>AI & Machine Learning</h3>
-                <h3 className={styles.h3}>Automation</h3>
                 <h3 className={styles.h3}>Cloud Intergation</h3>
                 <h3 className={styles.h3}>Full Stack Development</h3>
+                <h3 className={styles.h3}>Test Automation</h3>
             </div> 
-                <div className={styles.container}>
-                    <p className={styles.p1}>See How We Can Transform Your Small Business</p>
-                    <a href={mailtoLink}>
-                        <Button className={styles.button} color='secondary'sx={{ borderRadius: '0' }}variant="contained" endIcon={ <ArrowForward />}>
-                            Contact
-                        </Button>
-                    </a>
+            <div className={styles.container}>
+                <p className={styles.p1}>See How We Can Transform Your Small Business</p>
+                <a href={mailtoLink}>
+                    <Button className={styles.button} color='secondary'sx={{ borderRadius: '0' }}variant="contained" endIcon={ <ArrowForward />}>
+                        Contact
+                    </Button>
+                </a>
             </div> 
         </div> 
     );
